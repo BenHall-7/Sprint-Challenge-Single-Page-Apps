eec90650ef0a1819b7cc80aa316381ac61234b64
BenHall-7 <benjaminjahall@gmail.com>
2019-12-15 22:39:57 -0500

    Render character list to page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,30 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard(props) {
+let Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+
+  img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+  }
+`;
+
+export default function CharacterCard({character}) {
+  console.log(character);
+
   return (
-    <div>
-      
-    </div>
+    <Wrapper>
+      <img src={character.image} alt={character.name}/>
+      <div>
+        <h2>{character.name}</h2>
+        {character.type && <p>Type: {character.type}</p>}
+        <p>Status: {character.status}</p>
+        <p>Species: {character.species}</p>
+      </div>
+    </Wrapper>
   )
 }
